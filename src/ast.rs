5d62ef5bb883cc8ef5a4b5f404cf39ea4ba7bb5d
zhanglinwei <lewiszlw520@gmail.com>
2023-09-15 18:03:37 +0800

    Define create table ast

--- src/ast.rs
+++ src/ast.rs
@@ -1,13 +1,34 @@
-#[derive(Debug)]
-pub enum Expr {
-    Number(i32),
-    Op(Box<Expr>, Opcode, Box<Expr>),
+pub enum Statement {
+    CreateTable(CreateTableStatement),
 }
 
+/// CREATE TABLE
 #[derive(Debug)]
-pub enum Opcode {
-    Mul,
-    Div,
-    Add,
-    Sub,
+pub struct CreateTableStatement {
+    pub name: Ident,
+    pub columns: Vec<ColumnDef>,
+}
+
+/// An identifier
+#[derive(Debug)]
+pub struct Ident {
+    /// The value of the identifier without quotes.
+    pub value: String,
+}
+
+/// Representation of a new column to define in a table
+#[derive(Debug)]
+pub struct ColumnDef {
+    pub name: Ident,
+    pub column_type: DataType,
+}
+
+/// SQL data types
+#[derive(Debug)]
+pub enum DataType {
+    /// Boolean
+    Boolean,
+
+    /// Tiny integer with optional display width e.g. TINYINT or TINYINT(3)
+    TinyInt(Option<u64>),
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,8 @@ use lalrpop_util::lalrpop_mod;
 
 pub mod ast;
 
-lalrpop_mod!(pub calculator); // synthesized by LALRPOP
+lalrpop_mod!(pub sql); // synthesized by LALRPOP
 
 #[test]
-fn calculator1() {
-    assert!(calculator::TermParser::new().parse("22").is_ok());
-    assert!(calculator::TermParser::new().parse("(22)").is_ok());
-    assert!(calculator::TermParser::new().parse("((((22))))").is_ok());
-    assert!(calculator::TermParser::new().parse("((22)").is_err());
-
-    assert!(calculator::ExprParser::new().parse("1 + 2 * 3").is_ok());
-    println!("{:?}", calculator::ExprParser::new().parse("1 + 2 * 3").unwrap());
-    println!("{:?}", calculator::ExprParser::new().parse("1 * 2 + 3").unwrap());
+fn test_create_table() {
 }
